fa1fc8c70d1a3ec3a605b35738a4a4c365d47c1e
BrookeGibbons <21301367@student.uwa.edu.au>
2019-05-27 11:28:00 +0800

    new script

--- Scripts/2.Format.and.upload.data.to.GlobalArchive.R
+++ Scripts/2.Format.and.upload.data.to.GlobalArchive.R
@@ -6,28 +6,115 @@ library(tidyr)
 library(dplyr)
 library(stringr)
 library(readr)
+library(purrr)
 
 # Set work directory ----
 work.dir=("C:/GitHub/Analysis_Miller_lobster") # For Brooke
 
 # Sub directories ----
 data.dir<-paste(work.dir,"Data",sep="/")
+uploads<-paste(data.dir,"Uploads",sep="/")
+
+# Functions to remove csv unfriendly thingos
+remove.commas <- function(c){
+  ( gsub(",", '.', c))
+}
+
+remove.colon <- function(c){
+  ( gsub(";", '.', c))
+}
+
+remove<-function(c){
+  ( gsub(" ", " ", c))
+}
 
 # Import data ----
 setwd(data.dir)
 dir()
 
-metadata<-read.csv("metadata.csv")%>%
+metadata.raw<-read.csv("metadata.csv")%>%
   filter(Source%in%c("oscar-doncel-canons-masters","ash-millers-masters"))%>%
   mutate(Source=str_replace_all(.$Source,c("oscar-doncel-canons-masters"="Dongara.Canons.Masters","ash-millers-masters"="Dongara.Millers.Masters")))%>%
-  mutate(CampaignID=paste(str_sub(Date,1,7),Source,"Trapping",sep="_")) # Make a more GlobalArchive-y CampaignID
+  dplyr::rename(Comment=Pot.remarks)%>%
+  mutate(Successful.count=ifelse(Exclude.pots%in%c("Yes"),"No","Yes"))%>%
+  mutate(Successful.length=ifelse(Exclude.pots%in%c("Yes"),"No","Yes"))%>%
+  mutate(Successful.count=ifelse(Comment%in%c("Pot Broke"),"No",Successful.count))%>%
+  mutate(Successful.length=ifelse(Comment%in%c("Pot Broke"),"No",Successful.length))%>%
+  select(-c(Exclude.pots))%>%
+  mutate(Time="12:00:00")%>%
+  mutate(Status="Fished")%>%
+  mutate(Depth=ifelse(!is.na(Depth.lidar),Depth.lidar,0))%>%
+  mutate(Observer=ifelse(Source%in%c("Dongara.Canons.Masters"),"Oscar Doncel Canon","Ash Miller"))%>%
+  glimpse()
 
-length<-read.csv("length.csv")%>%
-  select(-c(Source))%>%
+length.raw<-read.csv("length.csv")%>%
   rename(Length=Carapace.length)%>%
-  mutate(Family="Palinuridae",Genus="Panulirus",Species="cygnus")
+  mutate(Family="Palinuridae",Genus="Panulirus",Species="cygnus")%>%
+  filter(Source%in%c("oscar-doncel-canons-masters","ash-millers-masters"))%>%
+  mutate(Source=str_replace_all(.$Source,c("oscar-doncel-canons-masters"="Dongara.Canons.Masters","ash-millers-masters"="Dongara.Millers.Masters")))%>%
+  replace_na(list(Length=(-9999)))%>%
+    glimpse()
+
+# Make a more GlobalArchive-y CampaignID
+campaigns<-metadata.raw%>%
+  distinct(Trip,Source,Date)%>%
+  group_by(Trip,Source)%>%
+  arrange(desc(Date))%>%
+  slice(n())%>%
+  ungroup()%>%
+  mutate(CampaignID=paste(str_sub(Date,1,7),Source,"Trapping",sep="_"))%>%
+  select(-c(Date))
+
+# Add campaigns to metadata and length ----
+metadata<-left_join(metadata.raw,campaigns)%>%
+  mutate(Date=str_replace_all(.$Date,c("-"=""))) # fix format of date here - cant do above as need it to make campaignid
+
+length<-left_join(length.raw,campaigns)
+
+# Create count (only use if GlobalArchive won't let me upload without count)
+count<-length%>%
+  group_by(CampaignID,Sample,Trip,Family,Genus,Species)%>%
+  summarise(Count=sum(Count))%>%
+  ungroup()
+
+# Remove csv unfriendly tings (e.g. commas) from metadata
+metadata[] <- sapply(metadata, remove.commas)
+metadata[] <- sapply(metadata, remove.colon)
+metadata[] <- sapply(metadata, remove)
+
+
 
 # Split data into campaigns based on trip ---
 # Metadata must have: Sample,	Latitude,	Longitude,	Date,	Time,	Location,	Status,	Site,	Depth,	Observer,	Successful.count	Succussful.length
 # Length file must have: Sample, Family, Genus, Species, Count, Length
 # Count file must have: Sample,	Family,	Genus,	Species,	Count
+setwd(uploads)
+dir()
+
+campaigns <- unique(unlist(metadata$CampaignID))
+
+for (i in 1:length(campaigns)){
+  # Metadata
+  temp.met <- subset(metadata, CampaignID == campaigns[i])
+  id<-temp.met$CampaignID
+  # Remove CampaignID from dataframe and then remove all columns where all are NA
+  temp.met2<-temp.met%>%select(-c(CampaignID))
+  temp.met3<-temp.met2[,which(unlist(lapply(temp.met2, function(x)!all(is.na(x)))))]
+  # Make an empty data frame to bring back in any columns that were removed but need to be in the data
+  columns<-c("Sample","Latitude","Longitude","Date","Time","Location","Status","Site","Depth","Observer","Successful.count","Successful.length","Comment")%>%map_dfr( ~tibble(!!.x := logical() ) )
+  # Add back in cols that may have been deleted
+  temp.met4<-bind_rows(columns, temp.met3)
+  # write file
+  write.csv(temp.met4, file=paste(unique(id),"_Metadata.csv",sep=""), quote=FALSE,row.names = FALSE,na = "")
+
+  # Length
+  temp.length <- subset(length, CampaignID == campaigns[i])%>%select(-c(CampaignID)) # Remove CampaignID from dataframe
+  write.csv(temp.length, file=paste(unique(id),"_Length.csv",sep=""), quote=FALSE,row.names = FALSE,na = "") # write file
+  
+  # Count
+  temp.count <- subset(count, CampaignID == campaigns[i])%>%select(-c(CampaignID)) # Remove CampaignID from dataframe
+  write.csv(temp.count, file=paste(unique(id),"_Count.csv",sep=""), quote=FALSE,row.names = FALSE,na = "") # write file
+}
+
+
+
