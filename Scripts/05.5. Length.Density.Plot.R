2a84e7380b86c20e60a9096102781399c76afdb5
BrookeGibbons <21301367@student.uwa.edu.au>
2019-06-25 11:47:36 +0800

    changes

--- Scripts/05.5. Length.Density.Plot.R
+++ Scripts/05.5. Length.Density.Plot.R
@@ -33,9 +33,9 @@ scaleFUN <- function(x) sprintf("%.0f", x)
 setwd(data.dir)
 dir()
 
-dat<- read_csv("length.sw.sst.csv")%>% 
+dat<- read.csv("length.sw.sst.csv")%>% 
   #change locations to four
-  mutate(Location=str_replace_all(.$Location, c("Little Horseshoe"="Boundary", "Golden Ridge"="Boundary", "Irwin Reef"="Mid", "White Point"="Mid", "Cliff Head"="Low-catch", "Seven Mile"= "Control")))%>%
+  mutate(Location=str_replace_all(.$Location, c("Little Horseshoe"="Boundary", "Golden Ridge"="Boundary", "Irwin Reef"="Mid", "White Point"="Mid", "Cliff Head"="Low-Catch", "Seven Mile"= "Control")))%>%
   drop_na(Colour)%>%
   drop_na(Carapace.length)%>%
   filter(Carapace.length>0)%>%
@@ -49,11 +49,16 @@ dat<- read_csv("length.sw.sst.csv")%>%
   select(Location, Site, Sample, Tag.number, Carapace.length, Sex, Colour)%>%
   glimpse()
 
+unique(dat$Location)
+
+
 #Removes doubles from Ben's SM data-----
 dat<- dat[!duplicated(dat[,c("Tag.number","Sample", "Carapace.length")]),]%>%
   glimpse
 
 dat<-as.data.frame(dat)
+
+unique(dat$Location)
 
 # Plotting Themes ----
 Theme1 <-
@@ -83,37 +88,56 @@ glimpse(mean)
 
 glimpse(dat)
 
+unique(dat$Location)
 dat_mean <- dat %>% 
   group_by(Location, Colour)%>% 
-  dplyr::summarise(mean = mean(Carapace.length))%>%
+  dplyr::summarise(mean = mean(Carapace.length),min=(mean(Carapace.length) - se(Carapace.length)),max=se.max(Carapace.length),median = median(Carapace.length),number=n())%>%
   glimpse()
+
+labels<-dat_mean%>%
+  mutate(label=paste("N (",Colour,"): ",number,sep=""))
 
 #Hitsograms----
 glimpse(dat)
 dat$Location<-factor(dat$Location, levels = c("Low-Catch","Boundary", "Mid", "Control"))
 glimpse(dat)
+unique(dat$Location)
 
 hist.stage<-ggplot(data = dat, aes(x = Carapace.length, fill = Colour)) + 
   geom_histogram(data = dplyr::filter(dat, Colour == "White"), aes(y = ..density..),col="grey30",binwidth=2) + 
   geom_histogram(data = dplyr::filter(dat, Colour == "Red"), aes(y = -..density..),col="grey30",binwidth=2) +
-  geom_point(data = dat_mean, aes(x = mean, y=0, shape=Colour), size=3.5)+
+  #geom_point(data = dat_mean, aes(x = mean, y=0, shape=Colour), size=3)+
+  geom_point(data = dplyr::filter(dat_mean, Colour == "Red"), aes(x = median, y=0, shape=Colour), size=3)+
+  geom_point(data = dplyr::filter(dat_mean, Colour == "White"), aes(x = median, y=0, shape=Colour), size=3)+
+  #geom_segment(data = dat_mean, aes(x=min,xend=max,y=0.02,yend=0.02))+
+  #geom_errorbarh(data=dplyr::filter(dat, Colour == "White"),aes(xmin=se.min(Carapace.length), xmax=se.max(Carapace.length),y=0),height = .92)+
+  #geom_errorbarh(data=dplyr::filter(dat, Colour == "Red"),aes(xmin=se.min(Carapace.length), xmax=se.max(Carapace.length),y=0),height = .92)+
+  #geom_linerange(data=dat_mean, aes(xmin=se.min, xmax=se.max))+
   scale_y_continuous(name='Relative density',breaks = seq(-0.08,0.08,0.04),labels=abs(seq(-8,8,4))) +
   # coord_flip()+
   theme_bw()+
   Theme1+
-  geom_vline(xintercept = 76)+
+  geom_vline(xintercept = 76, linetype = "dashed")+
   xlab("Carapace length (mm)")+
   scale_fill_manual(values = c("red","white"))+ #,name="Lobster colouration",labels=c("Red","White"
   geom_hline(aes(yintercept=0))+
   facet_grid(Location~.)+
   xlim(40,100)+
-  theme(legend.position="top") 
+  #ylim(-0.08,0.15)+
+  theme(legend.position="top")+
+  #annotate("text", x = 60, y = 0, label = paste("n red:",number))+
+  geom_text(data = dplyr::filter(labels, Colour == "White"), aes(x = 97, y = 0.07, label = label))+
+  geom_text(data = dplyr::filter(labels, Colour == "Red"), aes(x = 97, y = 0.05, label = label))
+
 hist.stage
+
+se.min(dat$Carapace.length)
+mean(dat$Carapace.length)
 
 #Save plots--
 setwd(plots.dir)
 
 
-ggsave(hist.stage,file="hist.location.stage.tall.png", width = 10, height = 16, units = "cm")
+ggsave(hist.stage,file="hist.location.stage.tall.median.png", width = 10, height = 16, units = "cm")
 
 
