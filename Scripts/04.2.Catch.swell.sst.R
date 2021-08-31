fa4930f9867ac7522c7e48aa9bcff4b7d4ccb0d6
BrookeGibbons <21301367@student.uwa.edu.au>
2021-08-31 09:30:05 +0800

    commit

--- Scripts/04.2.Catch.swell.sst.R
+++ Scripts/04.2.Catch.swell.sst.R
@@ -7,6 +7,21 @@ library(dplyr)
 library(stringr)
 library(lubridate)
 library(sf)
+library(weathermetrics)
+require(raster)
+require(tidyverse)
+require(devtools)
+require(sf)
+require(magrittr)
+require(ncdf4)
+require(lubridate)
+require(doParallel)
+require(parallel)
+require(foreach)
+require(tidync)
+require(doSNOW)
+require(tcltk)
+
 
 # Study name----
 study<-"Lobster.Data"
@@ -157,6 +172,81 @@ get.sst<-function(data){
   if(anyNA(data$sst)){print("Email matt and say: There are a lot of sst NAs. You need to increase the number of nearest points to search")}
   return(data$sst)
 } #Matts Function for sst data
+
+
+
+devtools::source_url("https://github.com/aodn/imos-user-code-library/blob/master/R/commons/NetCDF/ncParse.R?raw=TRUE")
+
+#get_sst
+
+#get_sst is a function to extract the sst given a series of Dates and points (Latitudes and Longitude). The function
+#returns a single column of sst corresponding to the input Dates and points.
+#sst is extracted from the AODN website and corresponds to a 6 day moving average. This was used to minimise holes
+#and for better reach into coastal areas. SST resolution is 0.02deg. The extraction function searches for data within 
+#100 and then 10000 and then 100000m from the point. Failing that it returns a warning
+#more details on source data at: https://catalogue-imos.aodn.org.au/geonetwork/srv/api/records/023ae12a-8c0c-4abc-997a-7884f9fec9cd
+
+#ARGUMENTS
+#Dates: is a data vector (created using as.Date) with format YYYY-MM-DD. 
+#Latitude: is the latitude of the point files in GDA94
+#Longitude is the longitude of the pont files in GDA94
+
+get_sst<- function(Dates, Long, Lat){
+  
+  Points<-tibble(Dates = Dates, Long = Long,  Lat = Lat) %>%  
+    sf::st_as_sf(coords = c("Long", "Lat"), crs = 4283) %>% #only extract for points on this day
+    st_transform(3112) %>%
+    dplyr::mutate(ID = row_number())
+  
+  #ggplot() +  geom_sf(data = Australia_map_Lambert) + geom_sf(data = Points) 
+  
+  Points <- Points#%>% mutate(land = as.integer(sf::st_intersects(geometry, Australia_map_Lambert))) #returns NA for water
+  
+  Points_extraction <- Points #%>% filter(is.na(land))
+  Date_code <- as.character(Points_extraction$Dates)
+  
+  Date_code <-Date_code%>% str_replace_all(., "-", "") %>% unique() #Extra Date codes for each day desired
+  
+  for(i in 1:length(Date_code)) { #loop through days download and extract data
+    print(i)
+    file_URL<- paste0("http://thredds.aodn.org.au/thredds/dodsC/IMOS/SRS/SST/ghrsst/L3S-6d/dn/", 
+                      substr(Date_code[i], 1,4), "/", 
+                      Date_code[i], 
+                      "212000-ABOM-L3S_GHRSST-SSTfnd-AVHRR_D-6d_dn.nc")
+    
+    dataset <- ncParse(file_URL, variables = "sea_surface_temperature")
+    
+    ## Extract data from variables and dimensions
+    lat <- dataset$dimensions$lat$data
+    lon <- dataset$dimensions$lon$data
+    temp <- dataset$variables$sea_surface_temperature$data
+    
+    ## Create a raster of longitude, latitude, and temperature data
+    dat1 <- list( )
+    dat1$x <- c( lon)
+    dat1$y <- c( lat)
+    dat1$z <- t( temp)
+    raster <- raster( dat1$z, xmn = range( dat1[[1]])[1], xmx = range( dat1[[1]])[2], ymn = range( dat1[[2]])[1], ymx = range( dat1[[2]])[2])
+    
+    crs(raster)<- CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')#in WGS84
+    
+    #Get points ready
+    temp <- Points_extraction %>% filter(Dates == ymd(Date_code[i])) %>% st_transform(4326) #only extract for points on this day
+    
+    temp$sst.100<-raster::extract(raster, temp, buffer = 100, fun=mean)
+    temp$sst.10000<-raster::extract(raster, temp, buffer = 10000, fun=mean)
+    temp$sst.100000<-raster::extract(raster, temp, buffer = 100000, fun=mean)
+    temp$sst <-ifelse(is.na(temp$sst.100), ifelse(is.na(temp$sst.10000), temp$sst.100000, temp$sst.10000), temp$sst.100)
+    
+    if(i==1){points_extracted<-temp}else{ points_extracted<-rbind(temp,points_extracted)}
+  }
+  points_extracted <- points_extracted%>% st_drop_geometry()
+  if( any(is.na(points_extracted$sst))) warning('raster extract returned NA consider increasing bufffer size')
+  Points <- Points%>% left_join(.,points_extracted[, c("ID", "sst")], by = c("ID" = "ID"))
+  Points$sst
+}
+
+
 # Import Catch data----
 setwd(data.dir)
 dir()
@@ -177,7 +267,7 @@ dat.swell.18 <-read.csv("JDW2018.csv")%>%
                 "Hs.m.sea"="Hs.m..sea",
                 "T1.s.sw"="T1.s..sw",
                 "T1.s.sea"="T1.s..sea")%>%
-  select(Date, Hs.m.sw, Hs.m.sea, T1.s.sw, T1.s.sea)%>%
+  dplyr::select(Date, Hs.m.sw, Hs.m.sea, T1.s.sw, T1.s.sea)%>%
   ungroup()%>%
   glimpse()
 
@@ -193,7 +283,7 @@ dat.swell.17 <- read.csv("JDW2017.csv")%>%
                 "Hs.m.sea"="Hs.m..sea",
                 "T1.s.sw"="T1.s..sw",
                 "T1.s.sea"="T1.s..sea")%>%
-  select(Date, Hs.m.sw, Hs.m.sea, T1.s.sw, T1.s.sea)%>%
+  dplyr::select(Date, Hs.m.sw, Hs.m.sea, T1.s.sw, T1.s.sea)%>%
   ungroup()%>%
   glimpse()
 
@@ -216,12 +306,101 @@ catch.sst<-catch.swell%>%
   unite(SiteNo, c(Lat,Long), sep = "", remove = F)%>% 
   glimpse()
 
-catch.sst<-catch.sst[1:1000,]
 
-catch.sst$sst<-get_sst(catch.sst$Date, catch.sst$Long, catch.sst$Lat)
+
+sites <- catch.sst %>%
+  distinct(Date, Long, Lat) %>%
+  mutate(Date=as.character(Date))
+
+dir()
+recovered <-read.csv("dat.all.sst.recovered.by.mat.csv")
+
+recovered.sites <- recovered %>%
+  distinct(Date, Long, Lat) %>%
+  mutate(Date=as.character(Date))
+
+missing <- anti_join(sites,recovered.sites)
+unique(missing$Date)%>%sort
+
+recovered.brooke <- read.csv("catch.sw.sst.recovered.brooke.csv")%>%
+  glimpse()
+
+recovered.sites.brooke <- recovered.brooke %>%
+  distinct(Date, Longitude, Latitude) %>%
+  mutate(Date=as.character(Date))%>%
+  rename(Lat=Latitude,Long=Longitude)
+
+missing.brooke <- anti_join(sites,recovered.sites.brooke)
+
+unique(missing.brooke$Date)%>%sort()
+# 233 are from 2019 - after Ash submitted her thesis.
+# where do the other 148 come from??????
+381-233
+
+length(unique(catch.sst$Long))
+
+write.csv(catch.sst,"catch.sst.notworking.csv")
+
+
+#sub.sites.sst.100$sst.c<-sub.sites.sst.100$sst-273.15
+# ~ 2000 sites
+
+sub.sites.50<-sites[1:50,]
+sub.sites.sst.50<-sub.sites.50
+sub.sites.sst.50$sst<-get_sst(sub.sites.50$Date, sub.sites.50$Long, sub.sites.50$Lat)
+
+sub.sites.100<-sites[51:100,]
+sub.sites.sst.100<-sub.sites.100
+sub.sites.sst.100$sst<-get_sst(sub.sites.100$Date, sub.sites.100$Long, sub.sites.100$Lat)
+
+sub.sites.150<-sites[101:150,]
+sub.sites.sst.150<-sub.sites.150
+sub.sites.sst.150$sst<-get_sst(sub.sites.150$Date, sub.sites.150$Long, sub.sites.150$Lat)
+
+glimpse(sub.sites.sst)
+
+#df2<-split(sites, rep(1:1, each = 50)) 
+str(df2)
+glimpse(df2)
+summary(df2)
+
+score<-sapply(df2,get_sst())[1,] 
+
+score<-lapply(df2,get_sst(df2$Date,df2$Long,df2$Lat))[1,]
+
+score<-sapply(df2,get_sst())[1,]
+
+str(df2)
+
+split.sites <- split(sites, rep(1:43, each = 50)) # split data up into chunks that will run
+datalist = list() # create a blank data frame
+
+for (i in 1:6) { # check on a smaller number first
+  temp <- split.sites[[i]] # create data frame with one chunk
+  temp$sst<-get_sst(temp$Date, temp$Long, temp$Lat) # use Mats SST function
+  temp$i <- i  # keep track of iteration
+  datalist[[i]] <- temp # add it to your list
+  print(temp$i)
+}
+
+big_data = do.call(rbind, datalist) # combine into one dataframe
+write.csv(big_data,"1-6 of subset.csv")
+split.sites <- split(sites, rep(1:43, each = 50)) # split data up into chunks that will run
+datalist2 = list() # create a blank data frame
+
+for (i in 10:43) { # check on a smaller number first
+  temp <- split.sites[[i]] # create data frame with one chunk
+  temp$sst<-get_sst(temp$Date, temp$Long, temp$Lat) # use Mats SST function
+  temp$i <- i  # keep track of iteration
+  datalist2[[i]] <- temp # add it to your list
+  print(temp$i)
+}
+big_data2 = do.call(rbind, datalist2) # combine into one dataframe
+write.csv(big_data2,"10-43 of subset.csv")
+print(1)
 
 #Bring in SST data- using Mathew's function----
-catch.sst$sst<-get.sst(catch.sst) #Keeps getting an error: Server is down? Fixed!
+# catch.sst$sst<-get.sst(catch.sst) #Keeps getting an error: Server is down? Fixed!
 glimpse(catch.sst)
 
 #Find average
